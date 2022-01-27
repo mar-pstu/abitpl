cb77af401b1385e7ca829b205c987d380be52dc7
chomovva <chomovva@gmail.com>
2022-01-27 16:44:49 +0200

    блоки главной

---
+++ src/scripts/init-offers.js
@@ -0,0 +1,31 @@
+( function () {
+
+	let $sliders = jQuery( '#offers .slider-content' );
+	let $heading = jQuery( '#offers .heading' );
+
+	function Init( index, slider ) {
+		jQuery( slider ).slick( {
+			infinite: true,
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			dots: false,
+			prevArrow: '#' + jQuery( slider ).attr( 'id' ) + '-prev',
+			nextArrow: '#' + jQuery( slider ).attr( 'id' ) + '-next',
+		} );
+	}
+
+	function Toggle( event ) {
+		event.preventDefault();
+		console.log( 'click' );
+		let $button = jQuery( event.target );
+		let $tab = jQuery( $button.attr( 'href' ) );
+		$button.siblings().removeClass( 'active' );
+		$button.addClass( 'active' );
+		$tab.siblings().addClass( 'hide' );
+		$tab.removeClass( 'hide' );
+	}
+
+	$sliders.each( Init );
+	$heading.on( 'click', 'a[href^="#"]', Toggle );
+
+} )();
--- src/scripts/init-reviews.js
+++ src/scripts/init-reviews.js
@@ -1,14 +1,19 @@
 ( function () {
 
-	jQuery( '#reviews-content' ).slick( {
-		dots: false,
-		arrows: true,
-		fade: false,
-		appendDots: '#graduates-controls',
-		autoplay: false,
-		lazyLoad: 'ondemand',
-		slidesToShow: 1,
-		slidesToScroll: 1,
-	} );
+	let $sliders = jQuery( '#reviews .slider-content' );
+
+	function Init ( index, slider ) {
+		jQuery( slider ).slick( {
+			dots: false,
+			fade: false,
+			autoplay: false,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			prevArrow: '#reviews-prev',
+			nextArrow: '#reviews-next',
+		} );
+	}
+
+	$sliders.each( Init );
 
 } )();
